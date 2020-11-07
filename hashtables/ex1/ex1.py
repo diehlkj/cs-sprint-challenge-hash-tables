3ea68336d39331c7f98dfe02f83c8538fcc7c446
Ken Diehl <diehlkj.99@gmail.com>
2020-11-06 22:53:43 -0500

    Started ex1.py, 2 tests passing, need to handle 0 weight problem and duplicate weight problem

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,22 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
-    return None
+    correct_weights = []
+    theDict = {}
+    
+    for i in range(0, length - 1):
+        #        weight  :  index
+        theDict[weights[i]] = i
+        
+    for weight in weights:
+        if limit - weight in theDict:
+            if len(correct_weights) != 0 and weights[correct_weights[0]] < weight:
+                correct_weights.insert(0, theDict[weight])
+                # print("calc", correct_weights, "weight", weight, "\n\n")
+            else:
+                correct_weights.append(theDict[weight]) # index
+                # print("calc", correct_weights, "weight", weight)
+    
+    if len(correct_weights) != 0:
+        print(correct_weights)
+        return correct_weights
+    else:
+        return None
